15304a4844a333d75b328c72e7374b51b199c819
code-luke <visco.tawatawa@gmail.com>
2026-01-21 14:42:30 +0700

    count

--- Remote Control/main.py
+++ Remote Control/main.py
@@ -9,3 +9,11 @@ collections.MutableMapping = abc.MutableMapping
 
 from dronekit import connect,VehicleMode
 from pymavlink import mavutil
+
+def count(time):
+    for i in range(time):
+        print(i+1)
+        sleep(1)
+    print('lesgo!')
+count(2)
+#Arm
---
+++ dummy.py
@@ -0,0 +1,4 @@
+from time import sleep
+print(1, end=" ")
+sleep(1)
+print(2, end=" ")
