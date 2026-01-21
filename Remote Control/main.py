2a6bf400d0e00c4f4baee33419ca3763158ecfcc
code-luke <visco.tawatawa@gmail.com>
2026-01-21 14:29:45 +0700

    Setup done

--- Remote Control/main.py
+++ Remote Control/main.py
@@ -6,3 +6,6 @@ import numpy as np
 from time import sleep
 from collections import abc
 collections.MutableMapping = abc.MutableMapping
+
+from dronekit import connect,VehicleMode
+from pymavlink import mavutil
