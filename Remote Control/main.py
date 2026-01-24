cceb6b8f6d55cd89c3d7d6dca9ed85db24f97e07
code-luke <visco.tawatawa@gmail.com>
2026-01-24 08:56:37 +0700

    change mode

--- Remote Control/main.py
+++ Remote Control/main.py
@@ -10,6 +10,7 @@ collections.MutableMapping = abc.MutableMapping
 from dronekit import connect,VehicleMode
 from pymavlink import mavutil
 
+#delay
 def count(time):
     for i in range(time):
         print(i+1)
@@ -21,3 +22,12 @@ count(2)
 def connect_vehicle():
     vehicle = connect("tcp:127.0.0.1:5762", wait_ready=True)
     return True
+
+#Change Mode
+def change_mode(vehicle, flight_mode):
+    vehicle.mode= VehicleMode(flight_mode) #Atur Mode
+    vehicle.wait_for_mode(flight_mode) #Nunggu mode berubah
+    if vehicle.mode.name == flight_mode:
+        print(f"Masuk mode {flight_mode}🫡")
+    else:
+        print("Gagal ganti mode😥")
