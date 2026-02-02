4b22d0bd7fc8c01657b3c7cd555ec5796b5e7886
code-luke <visco.tawatawa@gmail.com>
2026-02-02 15:33:16 +0700

    extract video/frame

---
+++ INTERN/Tugas Akhir/extract_frames.py
@@ -0,0 +1,49 @@
+import argparse #terjemahin python variable di terminal
+import cv2 #Computer Vision
+import os #Untuk ambil file
+
+#Extarct frames dari video
+def extract_frames(video_file, interval):
+    #Open video file
+    cap = cv2.VideoCapture(video_file)
+
+    if not cap.isOpened(): #Kalo videonya gabisa dibuka
+        print('Error: Unable to open  video file %s' % video_file)
+        return
+    
+    #Kasih Tempat buat nyimpen frame
+    video_name = os.path.splitext(os.path.basename(video_file))[0]
+    output_dir = video_name
+
+    if not os.path.exists(output_dir): #Kalo directori/tempat belum ada
+        os.makedirs(output_dir) #buat directori
+
+    frame_count= 0 
+    extracted_count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if frame_count % interval == 0:
+            frame_filename = '%s/frame_%04d.jpg' % (output_dir, extracted_count)
+            cv2.imwrite(frame_filename, frame)
+            extracted_count += 1
+
+        frame_count += 1
+
+    cap.release()
+    print(f"Selesai. {extracted_count} frame diekstrak.")
+
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description="Extract frame dari video")
+        parser.add_argument("video", help="Path ke file video")
+        parser.add_argument(
+            "--interval",
+            type=int,
+            default=30,
+            help="Ambil 1 frame tiap N frame"
+        )
+
+    args = parser.parse_args()
+    extract_frames(args.video, args.interval)
