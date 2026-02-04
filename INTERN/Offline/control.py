d070b03bc666af768089e42e13d5fcd8d7f83b81
code-luke <visco.tawatawa@gmail.com>
2026-02-04 13:59:18 +0700

    setup tugas magang offline

---
+++ INTERN/Offline/control.py
@@ -0,0 +1,10 @@
+import collections
+import cv2
+import time
+import numpy as np
+
+from collections import abc
+collections.MutableMapping = abc.MutableMapping
+
+from pymavlink import mavutil
+from dronekit import VehicleMode,connect
---
+++ INTERN/Offline/eksekusi.py
@@ -0,0 +1,19 @@
+#Sesuai dengan rencana yang sudah kubuat
+# •	Takeoff dari Start
+# •	Maju ke Wp 1, kecepatan 0.2 m/s
+# •	Yaw -90 derajat
+# •	Maju ke Wp 2, kecepatan 0.6 m/s
+# •	Yaw 90 derajat
+# •	Maju ke Wp 3, kecepatan 0.4 m/s
+# •	Maju ke Wp 4, kecepatan 0.4 m/s
+# •	Maju ke Wp 5, keccepatan 0.4 m/s
+# •	Maju ke Wp 6, kecepatan 0.4 m/s
+# •	Yaw 90 derajat
+# •	Maju ke Wp 7, kecepatan 0.4 m/s
+# •	Maju ke Wp 8, kecepatan 0.4 m/s
+# •	Yaw -90 derajat
+# •	Maju ke Wp 9, kecepatan 0.4 m/s
+# •	Maju ke Wp 10, kecepatan 0.4 m/s
+# •	Yaw 90 derajat
+# •	Maju menuju Finish, kecepatan 0.6 m/s
+
