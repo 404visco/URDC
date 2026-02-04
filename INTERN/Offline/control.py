cf01f8c6d883db48b2049cff5fa988b7bcb655d9
code-luke <visco.tawatawa@gmail.com>
2026-02-04 14:02:57 +0700

    connect

--- INTERN/Offline/control.py
+++ INTERN/Offline/control.py
@@ -8,3 +8,10 @@ collections.MutableMapping = abc.MutableMapping
 
 from pymavlink import mavutil
 from dronekit import VehicleMode,connect
+
+def connect_vehicle(vehicle):
+    vehicle = connect("tcp:127.0.0.1:5762", wait_ready=True)
+    return vehicle
+
+def change_mode(vehicle, flight_mode):
+    
